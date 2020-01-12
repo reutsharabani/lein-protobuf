90214718fb8a78eec7f4a0a235260a4e142d3fa0
Reut Sharabani <reut.sharabani@gmail.com>
2020-01-12 15:19:51 +0200

    release for clojure 1.10

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-protobuf "0.5.0"
+(defproject lein-protobuf "0.6.0"
   :description "Leiningen plugin for compiling protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/ninjudd/protobuf"
+  :url "https://github.com/reutsharabani/lein-protobuf"
   :dependencies [[fs "1.2.0"]
                  [conch "0.2.0"]
                  [leinjacker "0.4.3"]]
